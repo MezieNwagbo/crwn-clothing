1bbbd676096d27f255fdda225be0b1e7dbd17d8c
ChimezieNwagbo <chimezienwagbotech@gmail.com>
2022-08-10 18:59:21 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import Shop from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="/shop" element={<Shop />}></Route>
-        </Routes>
-      </BrowserRouter>
-
-      {/* create your routes to other pages here */}
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
