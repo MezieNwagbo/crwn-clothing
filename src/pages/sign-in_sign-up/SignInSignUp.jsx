cc0cbfc1fb515e052a760848885ed442823938dd
ChimezieNwagbo <chimezienwagbotech@gmail.com>
2022-08-22 21:18:26 +0100

    added email auth

--- src/pages/sign-in_sign-up/SignInSignUp.jsx
+++ src/pages/sign-in_sign-up/SignInSignUp.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 import './SignInSignUp.scss';
 
-const SignInSignUp = () => <SignIn />;
+const SignInSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SignInSignUp;
